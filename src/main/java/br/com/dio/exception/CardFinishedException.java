509d35acd09f11be128e95d18b04c9cffb27c844
Carla Albuquerque <ccja@cesar.school>
2025-03-18 11:43:02 -0300

    feat: primeira integração com banco de dados

---
+++ src/main/java/br/com/dio/exception/CardFinishedException.java
@@ -0,0 +1,9 @@
+package br.com.dio.exception;
+
+public class CardFinishedException extends RuntimeException {
+
+    public CardFinishedException(String message) {
+
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/dio/persistence/converter/OffsetDateTimeConverter.java
@@ -0,0 +1,23 @@
+package br.com.dio.persistence.converter;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+
+import static java.time.ZoneOffset.UTC;
+import static java.util.Objects.nonNull;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class OffsetDateTimeConverter {
+
+    public static OffsetDateTime toOffsetDateTime(final Timestamp value){
+        return nonNull(value) ? OffsetDateTime.ofInstant(value.toInstant(), UTC ): null;
+    }
+
+    public static  Timestamp toTimestamp(final OffsetDateTime value){
+        return nonNull(value) ? Timestamp.valueOf(value.atZoneSameInstant(UTC).toLocalDateTime()) : null;
+    }
+
+}
